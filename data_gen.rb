6b901e1ec5d320c900a3e5378ac1410aef2050a0
Mateo Carreras <mateo.carreras@gmail.com>
2023-08-12 20:45:08 -0700

    Change test data to include floating point numbers and multiple sensors per row

--- data_gen.rb
+++ data_gen.rb
@@ -1,5 +1,15 @@
 DATA_TO_WRITE = [
-    3, 0, 0, 0, 7, 3, 0, 0, 0, 9
+    # integer_test
+    3,
+    0, 0, 0, 7,
+    # integer_test
+    3,
+    0, 0, 0, 9,
+    # LSM
+    1,
+    0x3F, 0, 0, 0, # 0.5, x
+    0x3F, 0, 0, 0, # 0.5, y
+    0x3F, 0, 0, 0, # 0.5, z
 ]
 FILE_PATH = 'data.bin'
 
@@ -9,4 +19,6 @@ File.open(FILE_PATH, 'wb') do |file|
 end
 
 # Output value of 0.5 in binary 32-bit float
-puts [0.5].pack('f')
+puts [0.5].pack('f').unpack('B*')
+# Output value of 0.5 in binary 64-bit float
+puts [0.5].pack('d').unpack('B*')
